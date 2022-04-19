e77524d04e9a7274ff9b057c8ab4c98d0bce517f
Masa919 <mo.919.garyotensei@gmail.com>
2022-04-20 06:58:18 +0900

    first commit

---
+++ WhiteStoriesBot.py
@@ -0,0 +1,94 @@
+# インストールした discord.py を読み込む
+import discord
+import random
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 動作中に必要なオブジェクトの生成
+
+num_reac_list = ["1⃣","2⃣","3⃣","4⃣","5⃣","6⃣"]
+num_emoji_list = [":one:",":two:",":three:",":four:",":five:",":six:"]
+player_info = [["Unknown"]*6,[4]*6,["Normal"]*6,["S"]*6,[[]]*6]
+mode_flg = [False]
+msg_list = [] #0:board_msg 1:history_msg 2:pre_msg
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+    
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    ch = message.channel
+    
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        if bool(message.embeds):
+            msg_list.append(message)
+        return
+    elif ".ws n" == message.content:
+        mode_flg[0] = True
+        embed = discord.Embed(title = "白物語", description = "百話目の怪談を語り終えたとき、私たちは霊障に見舞われるだろう。\n怪談を面白く「訂正」して、霊障を回避しよう", color=0xffffff)
+        for i in range(6):
+            embed.add_field(name="Unknown",value="NG Role:" + num_reac_list[i] +"\nHP:4\nStatus:Normal\nS→")
+        embeded_msg = await ch.send(embed=embed)
+            
+        for i in range(6):
+            await embeded_msg.add_reaction(num_reac_list[i])
+        history_msg = await ch.send("history:\n")
+        pre_bot_msg = await ch.send("START!!")
+        
+        msg_list.append(history_msg)
+        msg_list.append(pre_bot_msg)
+    elif '.ws d' == message.content and mode_flg[0] and player_info[2][player_info[0].index(message.author.name)] == "Normal":
+        delete_msg = msg_list[2]
+        
+        emb = msg_list[0].embeds[0]
+        num = random.randint(1,6)
+        reply = f'{message.author.mention}:' + str(num)
+        msg_list[2] = await ch.send(reply)
+        await delete_msg.delete()
+        player_index = player_info[0].index(message.author.name)
+        player_info[4][player_index].append(num)
+        if len(player_info[4][player_index]) % 5 == 0:
+            player_info[3][player_index] = player_info[3][player_index] + "→" + str(num) + "\n"
+        else:
+            player_info[3][player_index] = player_info[3][player_index] + "→" + str(num) 
+        if player_index == num - 1:
+            player_info[1][player_index] -= 1
+        if player_info[1][player_index] == 0:
+            player_info[2][player_index] = "Mad"
+
+        emb.set_field_at(player_index,name = player_info[0][player_index],value= "NG Role:" + str(num_reac_list[player_index]) +"\nHP:" + str(player_info[1][player_index]) + "\nStatus:Normal\n" + player_info[3][player_index])
+        await msg_list[0].edit(embed=emb)
+        await msg_list[1].edit(content = msg_list[1].content + reply + "→")
+
+        await message.delete()
+    else:
+        
+        return
+
+
+#リアクションがついたときの処理
+@client.event
+async def on_reaction_add(reaction,user):
+    msg_reac_added = reaction.message
+    embed_on_addedmsg = msg_reac_added.embeds[0]
+    if user.bot:
+        return
+    elif mode_flg[0]:
+        
+        user_index = num_reac_list.index(str(reaction.emoji))
+        player_info[0][user_index] = user.name
+        embed_on_addedmsg.set_field_at(user_index, name=user.name,value= "NG Role:" + str(reaction) +"\nHP:4\nStatus:Normal\nS→")
+        await msg_reac_added.edit(embed=embed_on_addedmsg)
+        await msg_reac_added.remove_reaction(reaction,user)
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
