457e55544256de8a7775659a718674e00d0581ca
Masa919 <44154274+Masa919@users.noreply.github.com>
2022-04-20 09:01:48 +0900

    Update WhiteStoriesBot.py

--- WhiteStoriesBot.py
+++ WhiteStoriesBot.py
@@ -3,7 +3,7 @@ import discord
 import random
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -13,12 +13,15 @@ client = discord.Client()
 num_reac_list = ["1⃣","2⃣","3⃣","4⃣","5⃣","6⃣"]
 num_emoji_list = [":one:",":two:",":three:",":four:",":five:",":six:"]
 player_info = [["Unknown"]*6,[4]*6,["Normal"]*6,["S"]*6,[[]]*6]
-mode_flg = [False]
-msg_list = [] #0:board_msg 1:history_msg 2:pre_msg
+mode_flg = []
+msg_list = [] 
+
+#0:board_msg 1:history_msg 2:pre_msg
 # 起動時に動作する処理
 @client.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
+    mode_flg.append(False)
     print('ログインしました')
     
 
@@ -32,8 +35,9 @@ async def on_message(message):
         if bool(message.embeds):
             msg_list.append(message)
         return
-    elif ".ws n" == message.content:
+    elif ".ws n" == message.content and not mode_flg[0] :
         mode_flg[0] = True
+        await message.delete()
         embed = discord.Embed(title = "白物語", description = "百話目の怪談を語り終えたとき、私たちは霊障に見舞われるだろう。\n怪談を面白く「訂正」して、霊障を回避しよう", color=0xffffff)
         for i in range(6):
             embed.add_field(name="Unknown",value="NG Role:" + num_reac_list[i] +"\nHP:4\nStatus:Normal\nS→")
@@ -46,30 +50,51 @@ async def on_message(message):
         
         msg_list.append(history_msg)
         msg_list.append(pre_bot_msg)
-    elif '.ws d' == message.content and mode_flg[0] and player_info[2][player_info[0].index(message.author.name)] == "Normal":
-        delete_msg = msg_list[2]
-        
-        emb = msg_list[0].embeds[0]
-        num = random.randint(1,6)
-        reply = f'{message.author.mention}:' + str(num)
-        msg_list[2] = await ch.send(reply)
-        await delete_msg.delete()
-        player_index = player_info[0].index(message.author.name)
-        player_info[4][player_index].append(num)
-        if len(player_info[4][player_index]) % 5 == 0:
-            player_info[3][player_index] = player_info[3][player_index] + "→" + str(num) + "\n"
+    elif '.ws d' == message.content and mode_flg[0]:
+        if player_info[2][player_info[0].index(message.author.name)] == "Mad":
+            await message.delete()
         else:
-            player_info[3][player_index] = player_info[3][player_index] + "→" + str(num) 
-        if player_index == num - 1:
-            player_info[1][player_index] -= 1
-        if player_info[1][player_index] == 0:
-            player_info[2][player_index] = "Mad"
+            delete_msg = msg_list[2]
 
-        emb.set_field_at(player_index,name = player_info[0][player_index],value= "NG Role:" + str(num_reac_list[player_index]) +"\nHP:" + str(player_info[1][player_index]) + "\nStatus:" + player_info[2][player_index] + "\n"　+ player_info[3][player_index])
-        await msg_list[0].edit(embed=emb)
-        await msg_list[1].edit(content = msg_list[1].content + reply + "→")
+            emb = msg_list[0].embeds[0]
+            num = random.randint(1,6)
+            reply = f'{message.author.mention}:' + str(num)
+            msg_list[2] = await ch.send(reply)
+            await delete_msg.delete()
+            player_index = player_info[0].index(message.author.name)
+            player_info[4][player_index].append(num)
+            if len(player_info[4][player_index]) % 5 == 0:
+                player_info[3][player_index] = player_info[3][player_index] + "→" + str(num) + "\n"
+            else:
+                player_info[3][player_index] = player_info[3][player_index] + "→" + str(num) 
+            if player_index == num - 1:
+                player_info[1][player_index] -= 1
+            if player_info[1][player_index] == 0:
+                player_info[2][player_index] = "Mad"
+            emb.set_field_at(player_index,name = player_info[0][player_index],value= "NG Role:" + str(num_reac_list[player_index]) +"\nHP:" + str(player_info[1][player_index]) + "\nStatus:" + player_info[2][player_index] + "\n" + player_info[3][player_index])
+            await msg_list[0].edit(embed=emb)
+            await msg_list[1].edit(content = msg_list[1].content + reply + "→")
 
+            await message.delete()
+    elif '.ws e' == message.content and mode_flg[0] :
         await message.delete()
+        await msg_list[2].delete()
+        await msg_list[1].delete()
+
+        await msg_list[0].clear_reactions()
+        await msg_list[0].delete()
+
+        player_info[0] = ["Unknown"]*6
+        player_info[1] = [4]*6
+        player_info[2] = ["Normal"]*6
+        player_info[3] = ["S"]*6
+        player_info[4] = [[]]*6
+        mode_flg.clear()
+        msg_list.clear() 
+        mode_flg.append(False)
+
+        
+        return #停止処理
     else:
         
         return
